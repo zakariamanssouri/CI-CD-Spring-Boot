62e47e477a357ed1ec13b23f8ef0b7b8b364f56b
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-30 21:20:19 +0100

    initial commit

---
+++ src/main/java/me/zakaria/CICDSpringBoot/CiCdSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.zakaria.CICDSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdSpringBootApplication.class, args);
+	}
+
+}
